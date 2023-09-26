1adaf86182722dfaee4dabb02b09d928203ae507
11pplgb <11.pplg.b.b@gmail.com>
2023-09-26 08:53:00 +0700

    26/9/23-1

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def hello_world():
  orang = {'nama': 'albardani', 'umur':'21thn'}
 
  komentar = [
-
+gi
   {'penulis': {'nama': 'Khalid'},'ucapan': 'hai albardani, artikel yang bagus'},
   {'penulis': {'nama': 'Ariqa'},'ucapan': 'artikel ini cukup membantu saya'}
  ]
